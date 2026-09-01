ba0b10a8c5a48f56048bf68776ab0c8703b77f4f
Pedro Henrique Sousa Lima <pedrophslima@gmail.com>
2026-09-01 16:38:43 -0300

    Implementa remoção de contato

--- agenda.py
+++ agenda.py
@@ -27,8 +27,15 @@ def buscar_contato() :
     if not encontrou:
         print("Nenhum contato encontrado.")
 
-def remover_contato() :
-    pass
+def remover_contato():
+    nome = input("Digite o nome exato do contato a remover: ")
+    for contato in contatos:
+        if contato["nome"].lower() == nome.lower():
+            contatos.remove(contato)
+            print("Contato removido.")
+            return
+    print("Contato não encontrado.")
+    
 
 while True:
     print ("\n=== Agenda de Contatos ===")
